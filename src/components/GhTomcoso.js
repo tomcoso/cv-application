e635966169d6cab4e0ada21455a66869a442104a
tomcoso <tomasdessy0@gmail.com>
2022-10-17 21:51:41 +0100

    add gh link

---
+++ src/components/GhTomcoso.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { FaGithub } from "react-icons/fa";
+
+class GhTomcoso extends React.Component {
+  render() {
+    return (
+      <footer>
+        <a href="https://github.com/tomcoso" target="_blank" rel="noreferrer">
+          <FaGithub size="2rem" />
+        </a>
+      </footer>
+    );
+  }
+}
+
+export default GhTomcoso;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import GhTomcoso from "./components/GhTomcoso";
 import "./style.scss";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
+    <GhTomcoso />
   </React.StrictMode>
 );
