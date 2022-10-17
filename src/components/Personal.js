a96bb10ff1dcd16534f9abf2bd3aea7cdc77d335
tomcoso <tomasdessy0@gmail.com>
2022-10-17 16:28:27 +0100

    implement edit functionality on Personal

--- src/components/Personal.js
+++ src/components/Personal.js
@@ -1,39 +1,124 @@
 import React from "react";
 
 class Personal extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      stateFirst: "",
+      stateLast: "",
+      dateOfBirth: "",
+      stateAddress: "",
+      stateEmail: "",
+      statePhone: "",
+    };
+
+    this.update = this.update.bind(this);
+  }
+
+  update(e) {
+    for (let each in this.state) {
+      if (e.target.id === each) {
+        let st = this.state;
+        let change = {};
+        change[each] = e.target.value;
+        let newSt = Object.assign(st, change);
+        this.setState(newSt);
+      }
+    }
+  }
+
   render() {
     return (
       <fieldset id="personal-info">
         <legend>Personal Information</legend>
 
         <div id="first-name">
-          {/* <label htmlFor="first-name">First Name</label> */}
-          <input type="text" placeholder="First Name" />
+          {this.props.mode === "edit" ? (
+            <input
+              id="stateFirst"
+              type="text"
+              placeholder="First Name"
+              value={this.state.stateFirst}
+              onChange={this.update}
+            />
+          ) : (
+            <span>{this.state.stateFirst}</span>
+          )}
         </div>
 
         <div id="last-name">
-          {/* <label htmlFor="last-name">Last Name</label> */}
-          <input type="text" placeholder="Last Name" />
+          {this.props.mode === "edit" ? (
+            <input
+              id="stateLast"
+              type="text"
+              placeholder="Last Name"
+              value={this.state.stateLast}
+              onChange={this.update}
+            />
+          ) : (
+            <span>{this.state.stateLast}</span>
+          )}
         </div>
 
         <div id="dob">
-          <label htmlFor="date-of-birth">DOB</label>
-          <input type="date" id="date-of-birth" min="16" max="100" />
+          {this.props.mode === "edit" ? (
+            <div>
+              <label htmlFor="date-of-birth">DOB</label>
+              <input
+                type="date"
+                id="dateOfBirth"
+                min="16"
+                max="100"
+                value={this.state.dateOfBirth}
+                onChange={this.update}
+              />
+            </div>
+          ) : (
+            <span>{this.state.dateOfBirth}</span>
+          )}
         </div>
 
         <div id="address">
-          {/* <label htmlFor="address">Address</label> */}
-          <input type="text" placeholder="Home Address" />
+          {this.props.mode === "edit" ? (
+            <input
+              type="text"
+              placeholder="Home Address"
+              id="stateAddress"
+              value={this.state.stateAddress}
+              onChange={this.update}
+            />
+          ) : (
+            <span>{this.state.stateAddress}</span>
+          )}
         </div>
 
         <div id="email">
-          {/* <label htmlFor="email">E-mail</label> */}
-          <input type="email" placeholder="E-mail" />
+          {this.props.mode === "edit" ? (
+            <input
+              type="email"
+              placeholder="E-mail"
+              id="stateEmail"
+              value={this.state.stateEmail}
+              onChange={this.update}
+            />
+          ) : (
+            <span>{this.state.stateEmail}</span>
+          )}
         </div>
 
         <div id="phone-number">
-          {/* <label htmlFor="phone-number">Phone Number</label> */}
-          <input type="tel" placeholder="Phone Number" />
+          {this.props.mode === "edit" ? (
+            <input
+              type="tel"
+              placeholder="Phone Number"
+              id="statePhone"
+              value={this.state.statePhone}
+              onChange={this.update}
+            />
+          ) : (
+            <span>{this.state.statePhone}</span>
+          )}
         </div>
       </fieldset>
     );
