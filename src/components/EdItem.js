6e88e042d1946b16dd7f236f0d3948959b1e714f
tomcoso <tomasdessy0@gmail.com>
2022-10-20 15:41:14 +0100

    refactor Education & EdItem to functional comps

--- src/components/EdItem.js
+++ src/components/EdItem.js
@@ -1,67 +1,64 @@
 import React from "react";
 
-class EdItem extends React.Component {
-  render() {
-    const item = this.props.item;
-    return (
-      <li className="education" key={item.id}>
-        {this.props.mode === "edit" ? (
+const EdItem = (props) => {
+  const item = props.item;
+  return (
+    <li className="education" key={item.id}>
+      {props.mode === "edit" ? (
+        <input
+          id="degree"
+          type="text"
+          placeholder="Career"
+          value={item.degree}
+          onChange={(e) => {
+            props.edit(e, item);
+          }}
+        />
+      ) : (
+        <span>{item.degree}</span>
+      )}
+
+      {props.mode === "edit" ? (
+        <input
+          id="institution"
+          type="text"
+          placeholder="Institution"
+          value={item.institution}
+          onChange={(e) => {
+            props.edit(e, item);
+          }}
+        />
+      ) : (
+        <span>{item.institution}</span>
+      )}
+
+      {props.mode === "edit" ? (
+        <span>
           <input
-            id="degree"
-            type="text"
-            placeholder="Career"
-            value={item.degree}
+            id="start"
+            type="date"
+            value={item.start}
             onChange={(e) => {
-              this.props.edit(e, item);
+              props.edit(e, item);
+            }}
+          />{" "}
+          To{" "}
+          <input
+            id="finish"
+            type="date"
+            value={item.finish}
+            onChange={(e) => {
+              props.edit(e, item);
             }}
           />
-        ) : (
-          <span>{item.degree}</span>
-        )}
-
-        {this.props.mode === "edit" ? (
-          <input
-            id="institution"
-            type="text"
-            placeholder="Institution"
-            value={item.institution}
-            onChange={(e) => {
-              this.props.edit(e, item);
-            }}
-          />
-        ) : (
-          <span>{item.institution}</span>
-        )}
-
-        {this.props.mode === "edit" ? (
-          <span>
-            <input
-              id="start"
-              type="date"
-              value={item.start}
-              onChange={(e) => {
-                this.props.edit(e, item);
-              }}
-            />{" "}
-            To{" "}
-            <input
-              id="finish"
-              type="date"
-              value={item.finish}
-              onChange={(e) => {
-                this.props.edit(e, item);
-                this.changeState(e);
-              }}
-            />
-          </span>
-        ) : (
-          <span>
-            {item.start.slice(0, 4)} To {item.finish.slice(0, 4)}.
-          </span>
-        )}
-      </li>
-    );
-  }
-}
+        </span>
+      ) : (
+        <span>
+          {item.start.slice(0, 4)} To {item.finish.slice(0, 4)}.
+        </span>
+      )}
+    </li>
+  );
+};
 
 export default EdItem;
--- src/components/Education.js
+++ src/components/Education.js
@@ -1,136 +1,116 @@
-import React from "react";
+import React, { useState } from "react";
 import EdItem from "./EdItem";
 import uniqid from "uniqid";
 import "./Education.scss";
 
-class Education extends React.Component {
-  constructor(props) {
-    super(props);
+const Education = (props) => {
+  const [studies, setStudies] = useState([]);
+  const [current, setCurrent] = useState({
+    institution: "",
+    degree: "",
+    start: "",
+    finish: "",
+    id: uniqid(),
+  });
+  const forceUpdate = useState(1)[1];
 
-    this.state = {
-      studies: [],
-      currentEd: {
-        institution: "",
-        degree: "",
-        start: "",
-        finish: "",
-        id: uniqid(),
-        editItem: this.editItem.bind(this),
-      },
-    };
-
-    this.addEd = this.addEd.bind(this);
-    this.updateEd = this.updateEd.bind(this);
-    this.editItem = this.editItem.bind(this);
-  }
-
-  addEd() {
-    for (let each of Object.keys(this.state.currentEd)) {
-      if (this.state.currentEd[each] === "") return;
-    }
-
-    let arr = [].concat(this.state.currentEd, this.state.studies);
-
-    this.setState({
-      studies: arr,
-      currentEd: {
-        institution: "",
-        degree: "",
-        start: "",
-        finish: "",
-        id: uniqid(),
-        editItem: this.editItem.bind(this.currentEd),
-      },
-    });
-  }
-
-  updateEd(e) {
+  const update = (e) => {
     let newCurrent;
-    for (let each of Object.keys(this.state.currentEd)) {
+    for (let each of Object.keys(current)) {
       if (e.target.id === each) {
         const change = {};
         change[each] = e.target.value;
-        const stateCopy = this.state.currentEd;
+        const stateCopy = current;
         newCurrent = Object.assign(stateCopy, change);
       }
     }
-    this.setState({
-      currentEd: newCurrent,
-    });
-  }
+    setCurrent(newCurrent);
+    forceUpdate(Math.random());
+  };
 
-  editItem(e, item) {
-    const studies = this.state.studies;
+  const addEd = () => {
+    for (let each of Object.keys(current)) {
+      if (current[each] === "") return;
+    }
+
+    let arr = [].concat(current, studies);
+
+    setStudies(arr);
+
+    setCurrent({
+      institution: "",
+      degree: "",
+      start: "",
+      finish: "",
+      id: uniqid(),
+    });
+  };
+
+  const editItem = (e, item) => {
     for (let each of studies) {
       if (item.id === each.id) {
         each[e.target.id] = e.target.value;
+        const updateEd = studies;
+        setStudies(updateEd);
+        forceUpdate(Math.random());
       }
     }
-    this.setState({ studies });
-  }
+  };
 
-  render() {
-    const current = this.state.currentEd;
-    return (
-      <fieldset id="education">
-        <legend>Education</legend>
-        {this.props.mode === "edit" ? (
-          <div id="new-education">
-            <div id="new-ed-title">
-              <label htmlFor="degree">Title</label>
-              <input
-                type="text"
-                id="degree"
-                value={current.degree}
-                onChange={this.updateEd}
-              ></input>
-            </div>
-            <div id="new-ed-institution">
-              <label htmlFor="institution">Institution</label>
-              <input
-                type="text"
-                id="institution"
-                value={current.institution}
-                onChange={this.updateEd}
-              ></input>
-            </div>
-            <div id="new-ed-date">
-              <label htmlFor="start">From:</label>
-              <input
-                type="date"
-                id="start"
-                value={current.start}
-                onChange={this.updateEd}
-              ></input>
-              <label htmlFor="finish">To:</label>
-              <input
-                type="date"
-                id="finish"
-                value={current.finish}
-                onChange={this.updateEd}
-              ></input>
-            </div>
-            <button type="button" id="new-ed-add" onClick={this.addEd}>
-              Add
-            </button>
+  return (
+    <fieldset id="education">
+      <legend>Education</legend>
+      {props.mode === "edit" ? (
+        <div id="new-education">
+          <div id="new-ed-title">
+            <label htmlFor="degree">Title</label>
+            <input
+              type="text"
+              id="degree"
+              value={current.degree}
+              onChange={update}
+            ></input>
           </div>
-        ) : (
-          ""
-        )}
+          <div id="new-ed-institution">
+            <label htmlFor="institution">Institution</label>
+            <input
+              type="text"
+              id="institution"
+              value={current.institution}
+              onChange={update}
+            ></input>
+          </div>
+          <div id="new-ed-date">
+            <label htmlFor="start">From:</label>
+            <input
+              type="date"
+              id="start"
+              value={current.start}
+              onChange={update}
+            ></input>
+            <label htmlFor="finish">To:</label>
+            <input
+              type="date"
+              id="finish"
+              value={current.finish}
+              onChange={update}
+            ></input>
+          </div>
+          <button type="button" id="new-ed-add" onClick={addEd}>
+            Add
+          </button>
+        </div>
+      ) : (
+        ""
+      )}
 
-        <ul id="all-education">
-          {this.state.studies.map((item) => (
-            <EdItem
-              item={item}
-              key={item.id}
-              mode={this.props.mode}
-              edit={this.editItem}
-            />
-          ))}
-        </ul>
-      </fieldset>
-    );
-  }
-}
+      <ul id="all-education">
+        {studies.map((item) => (
+          <EdItem item={item} key={item.id} mode={props.mode} edit={editItem} />
+        ))}
+      </ul>
+    </fieldset>
+  );
+};
 
 export default Education;
